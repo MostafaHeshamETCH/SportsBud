901d4c74a6680c3915902a9f0c07663fbdc7a548
Etch <mostafah2000@live.com>
2021-09-06 15:30:06 +0200

    Adding sports types assets/images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import "./App.css";
-import NavBarTwoBtns from "./components/NavBarTwoBtns";
 import Homepage from "./components/Homepage";
-import useWindowDimensions from "./components/useWindowDimensions";
-import SignInPage from "./components/SignInPage";
-import SportsTypeChoice from "./components/SportsTypeChoice";
 
 function App() {
-  const { height } = useWindowDimensions();
-
-  const bgFullHeightStyle = {
-    height: height
-  };
-
   return (
-    <div className="bg-white font-all" style={bgFullHeightStyle}>
-      {/* <NavBarTwoBtns btnTxt="SIGN UP" /> */}
-      <SportsTypeChoice />
+    <div>
+      <Homepage />
     </div>
   );
 }
